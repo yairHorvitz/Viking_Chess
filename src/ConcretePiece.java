130eac9854ddc4f84e5837ea00cddc347ab6d96d
yair margalit <yairchai@gmail.com>
2024-01-21 18:30:49 +0200

    עידכון קבצים חדש

--- src/ConcretePiece.java
+++ src/ConcretePiece.java
@@ -4,7 +4,7 @@ public abstract class ConcretePiece implements Piece{
     private Player _owner;
     private String _ID;
     private int _number;
-    private ArrayList <Position> _positions = new ArrayList<>();
+    public ArrayList <Position> _positions = new ArrayList<>();
     public ConcretePiece (Player player , String id, int number, Position position){
         _owner = player;
         _ID = id;
@@ -18,7 +18,7 @@ public abstract class ConcretePiece implements Piece{
     public String getType() {return null;}
     public int get_number() {return _number;}
     public String get_ID() {return _ID;}
-    public Position get_LastPosition() {return _positions.get(_positions.size()-1);}//Getting the last member
+    public Position get_LastPosition() {return _positions.get(_positions.size() -1);}//Getting the last member
     public void AddPosition(Position p){_positions.add(p);}//Adding a member to arraylist
     public void getAllPositions (){for (Position position : _positions) {System.out.print("(" + position.get_x() +","+ position.get_y() + ")");}}
 }
